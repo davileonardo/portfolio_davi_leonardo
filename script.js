f685faf0546f1d43ceff4f209d54d3e439dd27be
Davi Leonardo Oliveira dos Santos <davileo17@gmail.com>
2026-01-07 15:18:56 -0300

    Atualização
    atualização

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 const galeria = document.getElementById("galeria");
-const totalImagens = 24; // coloque aqui a quantidade total de imagens que tem na pasta
+const modal = document.getElementById("modal");
+const modalImg = document.getElementById("modalImg");
+const closeBtn = document.querySelector(".close");
+
+const totalImagens = 24;
 
 for (let i = 1; i <= totalImagens; i++) {
   const img = document.createElement("img");
   img.src = `ArtsFeitas/1 (${i}).png`;
   img.alt = `Arte ${i}`;
+
+  img.addEventListener("click", () => {
+    modal.style.display = "flex";
+    modalImg.src = img.src;
+  });
+
   galeria.appendChild(img);
 }
+
+closeBtn.onclick = () => {
+  modal.style.display = "none";
+};
+
+modal.onclick = (e) => {
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+};
